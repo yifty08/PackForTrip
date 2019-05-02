a35d3f1aee8dd5ac25a30f8edd43d3327c1c6c7d
Anthony Valle <valleganthony@gmail.com>
2019-05-02 14:37:33 -0400

    Created constructors and Getters and setters for Footware

--- src/com/company/Footwear.java
+++ src/com/company/Footwear.java
@@ -1,4 +1,42 @@
 package com.company;
 
 public class Footwear {
+    private String typeOfFootware;
+    private double sizeOfShoe;
+    private String color;
+
+    public Footwear() {
+    }
+
+    public Footwear(String typeOfFootware, double sizeOfShoe, String color) {
+        this.typeOfFootware = typeOfFootware;
+        this.sizeOfShoe = sizeOfShoe;
+        this.color = color;
+    }
+
+    public String getTypeOfFootware() {
+        return typeOfFootware;
+    }
+
+    public void setTypeOfFootware(String typeOfFootware) {
+        this.typeOfFootware = typeOfFootware;
+    }
+
+    public double getSizeOfShoe() {
+        return sizeOfShoe;
+    }
+
+    public void setSizeOfShoe(double sizeOfShoe) {
+        this.sizeOfShoe = sizeOfShoe;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
 }
